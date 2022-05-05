1d57e537bd663361f317dd5e8255b144721abe9e
MinhHieu259 <mhieu7252@gmail.com>
2022-05-05 16:56:23 +0700

    tam ngưng 2

--- src/main/java/com/minhhieu/securityfullcourse/security/ApplicationSecurityConfig.java
+++ src/main/java/com/minhhieu/securityfullcourse/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.minhhieu.securityfullcourse.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import static com.minhhieu.securityfullcourse.security.ApplicationUserRole.*;
-
+import static com.minhhieu.securityfullcourse.security.ApplicationUserPermission.*;
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,9 +27,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -38,21 +44,31 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-       UserDetails minhhieuUser = User.builder()
+       UserDetails annaSmithUser = User.builder()
                 .username("minhhieu")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
-               .build();
+//                .roles(STUDENT.name())
+               .authorities(STUDENT.getGrantedAuthorities())
+                .build();
 
-       UserDetails hieunguyenUser = User.builder()
+       UserDetails lindaUser = User.builder()
                .username("hieunguyen")
                .password(passwordEncoder.encode("password123"))
-               .roles(ADMIN.name())
+//               .roles(ADMIN.name())
+               .authorities(ADMIN.getGrantedAuthorities())
                .build();
 
+        UserDetails tomUser = User.builder()
+                .username("hieu")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
         return new InMemoryUserDetailsManager(
-                minhhieuUser,
-                hieunguyenUser
+                annaSmithUser,
+                lindaUser,
+                tomUser
         );
     }
 }
--- src/main/java/com/minhhieu/securityfullcourse/student/StudentManagementController.java
+++ src/main/java/com/minhhieu/securityfullcourse/student/StudentManagementController.java
@@ -1,2 +1,40 @@
-package com.minhhieu.securityfullcourse.student;public class StudentManagementController {
+package com.minhhieu.securityfullcourse.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/management/api/v1/students")
+public class StudentManagementController {
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Minh Hieu"),
+            new Student(2, "Hieu Nguyen"),
+            new Student(3, "Hieu Dep Zai")
+    );
+
+    @GetMapping
+    public static List<Student> getAllStudents() {
+        System.out.println("getAllStudents");
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        System.out.println("registerNewStudent");
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId){
+        System.out.println("deleteStudent");
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
+        System.out.println("updateStudent");
+        System.out.println(String.format("%s %s", studentId, student));
+    }
 }
